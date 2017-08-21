d236f45a8c9b854769c74445a7986ded525ced88
Josh Hargreaves <joshyhargreaves@gmail.com>
2017-08-21 02:02:52 +0100

    Prettier + tidy up

--- tests/lib/rules/eslint-plugin-no-async-callback.js
+++ tests/lib/rules/eslint-plugin-no-async-callback.js
@@ -2,68 +2,66 @@
  * @fileoverview All async tests must be implemented with async functions not callbacks
  * @author Joshua Hargreaves
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/eslint-plugin-no-async-callback"),
-
-RuleTester = require("eslint").RuleTester;
+var rule = require('../../../lib/rules/eslint-plugin-no-async-callback'),
+  RuleTester = require('eslint').RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ASYNC_FUNC_ERROR = `Async tests must use async functions and async-await instead of 'done' arguments.`
+const ASYNC_FUNC_ERROR = `Async tests must use async functions and async-await instead of 'done' arguments.`;
 
 var ruleTester = new RuleTester();
-ruleTester.run("eslint-plugin-no-async-callback", rule, {
-
-    valid: [
-        `describe("describe1", function() {
+ruleTester.run('eslint-plugin-no-async-callback', rule, {
+  valid: [
+    `describe("describe1", function() {
             it("it1", function() {});
          });`,
-         `describe("describe2", function() {
+    `describe("describe2", function() {
             it("it1", function() {});
             it("it2", function() {});
          });`,
-         `it("it2", function() {});`,
-    ],
+    `it("it2", function() {});`,
+  ],
 
-    invalid: [
+  invalid: [
+    {
+      code: [
+        'describe("describe1", function() {',
+        'it("it1", function(done) {});',
+        '});',
+      ].join('\n'),
+      errors: [
         {
-            code: [
-                'describe("describe1", function() {',
-                    'it("it1", function(done) {});',
-                '});'
-            ].join('\n'),
-            errors: [
-              {
-                message: ASYNC_FUNC_ERROR,
-                column: 11,
-                line: 2,
-              },
-            ],
+          message: ASYNC_FUNC_ERROR,
+          column: 11,
+          line: 2,
+        },
+      ],
+    },
+    {
+      code: [
+        'it("it1", function(done) {});',
+        'it("it1", function(x) {});',
+      ].join('\n'),
+      errors: [
+        {
+          message: ASYNC_FUNC_ERROR,
+          column: 11,
+          line: 1,
         },
         {
-            code: [
-                'it("it1", function(done) {});',
-                'it("it1", function(x) {});'
-            ].join('\n'),
-            errors: [
-              {
-                message: ASYNC_FUNC_ERROR,
-                column: 11,
-                line: 1,
-              },
-              {
-                message: ASYNC_FUNC_ERROR,
-                column: 11,
-                line: 2,
-              },
-            ],
-        }
-    ]
+          message: ASYNC_FUNC_ERROR,
+          column: 11,
+          line: 2,
+        },
+      ],
+    },
+  ],
 });
