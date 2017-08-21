d2dcd68ff5da8a3b8251f55b2d0ef013e264d408
Josh Hargreaves <joshyhargreaves@gmail.com>
2017-08-21 01:54:47 +0100

    Added tests and implementation

--- tests/lib/rules/eslint-plugin-no-async-callback.js
+++ tests/lib/rules/eslint-plugin-no-async-callback.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview All sync tests must be implemented with async functions not callbacks
+ * @fileoverview All async tests must be implemented with async functions not callbacks
  * @author Joshua Hargreaves
  */
 "use strict";
@@ -10,12 +10,13 @@
 
 var rule = require("../../../lib/rules/eslint-plugin-no-async-callback"),
 
-    RuleTester = require("eslint").RuleTester;
-
+RuleTester = require("eslint").RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
+
+const ASYNC_FUNC_ERROR = `Async tests must use async functions and async-await instead of 'done' arguments.`
 
 var ruleTester = new RuleTester();
 ruleTester.run("eslint-plugin-no-async-callback", rule, {
@@ -23,25 +24,46 @@ ruleTester.run("eslint-plugin-no-async-callback", rule, {
     valid: [
         `describe("describe1", function() {
             it("it1", function() {});
-        });`
-
-        // give me some code that won't trigger a warning
+         });`,
+         `describe("describe2", function() {
+            it("it1", function() {});
+            it("it2", function() {});
+         });`,
+         `it("it2", function() {});`,
     ],
 
     invalid: [
         {
-            code: 
-              `describe("describe1", function() {
-                it("it1", function(done) {});
-               });`,
+            code: [
+                'describe("describe1", function() {',
+                    'it("it1", function(done) {});',
+                '});'
+            ].join('\n'),
             errors: [
               {
-                message: 'Test title is used multiple times in the same test suite.',
-                column: 4,
-                line: 3,
+                message: ASYNC_FUNC_ERROR,
+                column: 11,
+                line: 2,
               },
             ],
-          },
-
+        },
+        {
+            code: [
+                'it("it1", function(done) {});',
+                'it("it1", function(x) {});'
+            ].join('\n'),
+            errors: [
+              {
+                message: ASYNC_FUNC_ERROR,
+                column: 11,
+                line: 1,
+              },
+              {
+                message: ASYNC_FUNC_ERROR,
+                column: 11,
+                line: 2,
+              },
+            ],
+        }
     ]
 });
